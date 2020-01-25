c5371dc06b20ab1c8ca43db46b067cdd54fdcb91
subram43 <subram43@purdue.edu>
2020-01-25 12:29:06 -0500

    Initial Commit

---
+++ flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixter
+//
+//  Created by Aditya Subramaniam on 1/25/20.
+//  Copyright © 2020 subram43. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
